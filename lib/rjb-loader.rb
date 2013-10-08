940d14775338fbfa6d675f8f558885b3a59b8b39
Enzo Z <enzozuccolotto@gmail.com>
2013-10-08 20:29:40 -0300

    Fixing java.lang.NoClassDefFoundError error
    When we are testing our generated we get the following error:
    java.lang.NoClassDefFoundError: net/sf/jasperreports/engine/JasperCompileManager

--- lib/rjb-loader.rb
+++ lib/rjb-loader.rb
@@ -30,28 +30,28 @@ module RjbLoader
     attr_accessor :classpath
     attr_accessor :java_options
   end
-  
+
   class << self
     attr_accessor :before_load_blocks
     attr_accessor :after_load_blocks
     attr_accessor :config
   end
-  
+
   self.before_load_blocks = []
   self.after_load_blocks  = []
   self.config                   = Config.new
   self.config.classpath         = '.'
   self.config.java_options      = ['-Djava.awt.headless=true','-Xms128M', '-Xmx256M']
-  
+
   def self.before_load &block
-    self.before_load_blocks << block 
+    self.before_load_blocks << block
   end
-  
+
   def self.after_load &block
-    self.after_load_blocks << block 
+    self.after_load_blocks << block
   end
-  
-  def self.init    
+
+  def self.init
     before_load_blocks.each do |block|
       block.call(config)
     end
@@ -61,16 +61,16 @@ module RjbLoader
     else
       Rjb::load( config.classpath, config.java_options )
     end
-    
+
     after_load_blocks.each do |block|
       block.call(config)
     end
   end
 
   class Railtie < Rails::Railtie
-    config.after_initialize do
+    config.to_prepare do
       RjbLoader.init
     end
   end
-  
+
 end
